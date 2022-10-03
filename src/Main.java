89e92d0860f07f82004f2bf626eba1b58fd1e9c4
YuryShageev <schageev.yur@yandex.ru>
2022-10-03 23:40:11 +0300

    Пробую

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,23 @@ public class Main {
         for (int i = 0; i <= 10; i ++) {
             System.out.println("Итерация цикла " + i);
         }
+
+        //Задача 2
+        System.out.println("Task 2");
+        for (int a = 10; a >= 1; a --) {
+            System.out.println("Итерация цикла " + a);
+        }
+
+        //Задача 3
+        System.out.println("Task 3");
+        for (int b = 0; b < 17; b += 2) {
+            System.out.println("Итерация цикла " + b);
+        }
+
+        //Задача 4
+        System.out.println("Task 4");
+        for (float w = 10; w <= 10 && w >= -10; w --) {
+            System.out.println("Итерация цикла " + w);
+        }
     }
 }
